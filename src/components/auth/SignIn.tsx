3641fff03cf5ebdecf79d24815499d8085ab7315
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-03 18:55:55 +0100

    Refactor component

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Container } from '../Container';
+import { Title } from '../form/Title';
+import { FormInput } from '../form/FormInput';
+import { Submit } from '../form/Submit';
 
 export const SignIn = () => {
   return (
     <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
       <Container>
-        <form className="bg-secondary rounded p-6 w-72">
-          <h1 className="text-xl text-white font-semibold text-center">Sign In</h1>
-          <div className="flex flex-col-reverse">
-            <input
-              id="email"
-              type="text"
-              className="bg-transparent rounded border-2 border-dark-subtle focus:border-white p-1 w-full text-lg text-white outline-none peer"
-              placeholder="john@email.com"
-            />
-            <label
-              htmlFor="email"
-              className="font-semibold text-dark-subtle peer-focus:text-white transition self-start"
-            >
-              Email
-            </label>
+        <form className="bg-secondary rounded p-6 w-72 space-y-6">
+          <Title>Sign In</Title>
+          <FormInput name="email" placeholder="joe@example.com" label="Email" type="text" />
+          <FormInput name="password" placeholder="*********" label="Password" type="password" />
+          <Submit value="Sign In" />
+          <div className="flex justify-between">
+            <a className="text-dark-subtle hover:text-white transition" href="#">
+              Forget password
+            </a>
+            <a className="text-dark-subtle hover:text-white transition" href="#">
+              Sign up
+            </a>
           </div>
         </form>
       </Container>
