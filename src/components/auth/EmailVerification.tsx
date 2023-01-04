9139177d608b3f0ef66027155b336e7fbfbb9ca7
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-04 13:46:44 +0100

    Add useState,useRef, useEffect and handler methods

--- src/components/auth/EmailVerification.tsx
+++ src/components/auth/EmailVerification.tsx
@@ -1,12 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Container } from '../Container';
 import { Title } from '../form/Title';
 import { Submit } from '../form/Submit';
 
 const OTP_LENGTH = 6;
+let currentOTPIndex: number;
 
 export const EmailVerification = () => {
   const [otp, setOtp] = useState<string[]>(new Array(OTP_LENGTH).fill(''));
+  const [activeOtpIndex, setActiveOtpIndex] = useState<number>(0);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const focusNextInputField = (index: number) => {
+    setActiveOtpIndex(index + 1);
+  };
+
+  const focusPrevInputField = (index: number) => {
+    let nextIndex: number;
+    const diff = index - 1;
+    nextIndex = diff !== 0 ? diff : 0;
+    setActiveOtpIndex(nextIndex);
+  };
+
+  const handleOtpChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+
+    if (!value) {
+      focusPrevInputField(currentOTPIndex);
+    } else {
+      focusNextInputField(currentOTPIndex);
+    }
+    setOtp((prevOtp) => {
+      const newOtp = [...prevOtp];
+      newOtp[currentOTPIndex] = value.substring(value.length - 1, value.length);
+      return [...newOtp];
+    });
+  };
+  const handleKeyDown = ({ key }: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+    currentOTPIndex = index;
+    if (key === 'Backspace') {
+      focusPrevInputField(currentOTPIndex);
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [activeOtpIndex]);
 
   return (
     <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
@@ -20,7 +60,11 @@ export const EmailVerification = () => {
             {otp.map((_, index) => {
               return (
                 <input
+                  ref={activeOtpIndex === index ? inputRef : null}
                   key={index}
+                  value={otp[index] || ''}
+                  onChange={(e) => handleOtpChange(e)}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
                   type="number"
                   min="0"
                   max="9"
