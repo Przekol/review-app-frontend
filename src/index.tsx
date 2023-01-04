e0ba054d6ce776ce3bcf08e4c1fa9518f4207461
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-04 09:45:38 +0100

    Add routers

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,46 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { SignIn } from './components/auth/SignIn';
+import { SignUp } from './components/auth/SignUp';
+import { EmailVerification } from './components/auth/EmailVerification';
+import { ForgetPassword } from './components/auth/ForgetPassword';
+import { ConfirmPassword } from './components/auth/ConfirmPassword';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'auth/signin',
+        element: <SignIn />,
+      },
+      {
+        path: 'auth/signup',
+        element: <SignUp />,
+      },
+      {
+        path: 'auth/verification',
+        element: <EmailVerification />,
+      },
+      {
+        path: 'auth/forget-password',
+        element: <ForgetPassword />,
+      },
+      {
+        path: 'auth/confirm-password',
+        element: <ConfirmPassword />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
 
