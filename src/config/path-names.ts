63704cff4359e46e298d935cd421a4fd9f00de0a
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-10 17:48:37 +0100

    Change pathsNames from object to enum

--- src/config/path-names.ts
+++ src/config/path-names.ts
@@ -1,11 +1,7 @@
-type Paths = {
-  [key: string]: string;
-};
-
-export const pathNames: Paths = {
-  AUTH_SIGN_IN: '/auth/sign-in',
-  AUTH_SIGN_UP: '/auth/sign-up',
-  AUTH_VERIFICATION: '/auth/verification',
-  AUTH_FORGET_PASSWORD: '/auth/forget-password',
-  AUTH_CONFIRM_PASSWORD: '/auth/confirm-password',
-};
+export enum PathNames {
+  AUTH_SIGN_IN = '/auth/sign-in',
+  AUTH_SIGN_UP = '/auth/sign-up',
+  AUTH_VERIFICATION = '/auth/verification',
+  AUTH_FORGET_PASSWORD = '/auth/forget-password',
+  AUTH_CONFIRM_PASSWORD = '/auth/confirm-password',
+}
